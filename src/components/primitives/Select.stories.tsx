42b848a53a3d1c45c9cbe095ef7f749aee5c677b
toll anbesu <tolaanbesu@gmail.com>
2025-10-29 18:57:31 +0300

    created story for select

---
+++ src/components/primitives/Select.stories.tsx
@@ -0,0 +1,29 @@
+
+import { Select, SelectOption } from "./Select";
+import { Meta, StoryObj } from "@storybook/react";
+
+const options: SelectOption[] = [
+  { label: "Option 1", value: "1" },
+  { label: "Option 2", value: "2", disabled: true },
+];
+
+// Default export with component metadata
+const meta: Meta<typeof Select> = {
+  title: "Primitives/Select",
+  component: Select,
+};
+
+export default meta;
+
+// Named stories
+export const Default: StoryObj<typeof Select> = {
+  args: {
+    options: options,
+  },
+};
+
+export const WithDisabledOption: StoryObj<typeof Select> = {
+  args: {
+    options: options,
+  },
+};
